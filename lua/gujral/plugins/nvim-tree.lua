01b331372410e6fcce8fc3f9f188c6fe406496f6
Ansh Gujral <gujralanshg@gmail.com>
2026-09-19 00:54:55 +0530

    add: todo-comments, edit(lazy): no notification on load, edit(nvim-tree): tree on right side

--- lua/gujral/plugins/nvim-tree.lua
+++ lua/gujral/plugins/nvim-tree.lua
@@ -3,13 +3,17 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 return {
-  "nvim-tree/nvim-tree.lua",
-  lazy = false,
-  opts = {},
-  keys = {
-    { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Toggle NvimTree" },
-  },
-  dependencies = {
-    "nvim-tree/nvim-web-devicons"
-  }
+	"nvim-tree/nvim-tree.lua",
+	lazy = false,
+	opts = {
+		view = {
+			side = "right",
+		},
+	},
+	keys = {
+		{ "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Toggle NvimTree" },
+	},
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
 }
